b6ec37fd804abccf78b0005eb2e005bfbd10e116
Charlotte <Charlotte@m.com>
2013-12-22 22:03:11 +0800

    Initial Project

---
+++ Angularjs-Spring_T1/src/main/java/com/bluedream/test/angular/service/RailwayStationService.java
@@ -0,0 +1,21 @@
+package com.bluedream.test.angular.service;
+
+import com.bluedream.test.angular.beans.RailwayStation;
+
+import java.util.List;
+
+
+public interface RailwayStationService {
+	
+    public List<RailwayStation> getAllRailwayStations();
+
+    public RailwayStation getRailwayStationById(Long id);
+
+    public void addRailwayStation(RailwayStation RailwayStation);
+
+    public void deleteRailwayStationById(Long id);
+
+    public void deleteAll();
+
+    public void updateRailwayStation(RailwayStation RailwayStation);
+}
